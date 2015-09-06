ceedc88a1d9c34a47259367d2f82883820d3b666
Phil Freeman <paf31@cantab.net>
2015-09-05 20:35:17 -0700

    Improved test

--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,11 @@ module Test.Main where
 
 import Prelude
 
+import Data.Maybe
+import Data.Either
 import Data.Lens
 import Data.Tuple
+import Data.Traversable (Traversable)
 
 import Control.Monad.Eff.Console
 
@@ -13,5 +16,15 @@ foo = lens _.foo (_ { foo = _ })
 bar :: forall a b r. Lens { bar :: a | r } { bar :: b | r } a b
 bar = lens _.bar (_ { bar = _ })
 
-main = do
-  log $ view (foo <<< bar) { foo: { bar: "Hello, World!"} }
+_Just :: forall a b r. Prism (Maybe a) (Maybe b) a b
+_Just = prism Just (maybe (Right Nothing) Left)
+
+type Foo a = { foo :: Maybe { bar :: Array a } }
+
+doc :: Foo String
+doc = { foo: Just { bar: [ "Hello", "World" ]} }
+
+bars :: forall a b. Traversal (Foo a) (Foo b) a b
+bars = foo <<< _Just <<< bar <<< traverse
+
+main = traverseOf bars log doc
