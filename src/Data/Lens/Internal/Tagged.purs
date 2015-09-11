84f4285ed4c8359c2e67ab887ef2c437d878915d
Lukas Heidemann <lukasheidemann@gmail.com>
2015-09-11 15:22:37 +0200

    `review` and `preview` for prisms.

---
+++ src/Data/Lens/Internal/Tagged.purs
@@ -0,0 +1,19 @@
+-- | This module defines the `Tagged` profunctor
+
+module Data.Lens.Internal.Tagged where
+
+import Data.Profunctor
+import Data.Profunctor.Choice
+import Data.Either
+
+newtype Tagged a b = Tagged b
+
+instance taggedProfunctor :: Profunctor Tagged where
+  dimap _ g (Tagged x) = Tagged (g x)
+
+instance taggedChoice :: Choice Tagged where
+  left  (Tagged x) = Tagged (Left x)
+  right (Tagged x) = Tagged (Right x)
+
+unTagged :: forall a b. Tagged a b -> b
+unTagged (Tagged x) = x
--- src/Data/Lens/Prism.purs
+++ src/Data/Lens/Prism.purs
@@ -1,11 +1,16 @@
 -- | This module defines functions for working with lenses.
 
 module Data.Lens.Prism where
-    
+
 import Prelude
-    
+
 import Data.Either
+import Data.Profunctor.Star
+import Data.Const
+import Data.Maybe
+import Data.Maybe.First
 import Data.Lens.Types
+import Data.Lens.Internal.Tagged
 import Data.Profunctor (dimap, rmap)
 import Data.Profunctor.Choice (left)
 
@@ -13,3 +18,10 @@ import Data.Profunctor.Choice (left)
 prism :: forall s t a b. (b -> t) -> (s -> Either a t) -> Prism s t a b
 prism to fro pab = dimap fro (either id id) (left (rmap to pab))
 
+-- | Review a value through a `Prism`.
+review :: forall s t a b. Prism s t a b -> b -> t
+review p = unTagged <<< p <<< Tagged
+
+-- | Previews the value of a `Prism`, if there is any.
+preview :: forall s t a b. Prism s t a b -> s -> Maybe a
+preview p = runFirst <<< getConst <<< runStar (p (Star (Const <<< pure)))
