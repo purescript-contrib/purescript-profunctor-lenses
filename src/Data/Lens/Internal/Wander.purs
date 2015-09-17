7f47130e2c28a9dc4b9328ce9ce45dbc3371b57b
Lukas Heidemann <lukasheidemann@gmail.com>
2015-09-17 18:01:12 +0200

    Some new folds and traversals.

--- src/Data/Lens/Internal/Wander.purs
+++ src/Data/Lens/Internal/Wander.purs
@@ -1,19 +1,21 @@
 -- | This module defines the `Wander` type class, which is used to define `Traversal`s.
 
 module Data.Lens.Internal.Wander where
-    
+
 import Prelude
 
-import Data.Traversable (Traversable, traverse)
 import Data.Profunctor.Strong (Strong)
-import Data.Profunctor.Choice (Choice)
-import Data.Profunctor.Star (Star(..))
+import Data.Profunctor.Star (Star(..), runStar)
+import Data.Identity (Identity (..), runIdentity)
 
-class (Strong p, Choice p) <= Wander p where
-  wander :: forall t a b. (Traversable t) => p a b -> p (t a) (t b)
-  
+-- | Class for profunctors that support polymorphic traversals.
+class (Strong p) <= Wander p where
+  wander
+    :: forall f s t a b. (forall f. (Applicative f) => (a -> f b) -> s -> f t)
+    -> p a b -> p s t
+
 instance wanderFunction :: Wander Function where
-  wander = map
+  wander t f s = runIdentity $ t (Identity <<< f) s
 
 instance wanderStar :: (Applicative f) => Wander (Star f) where
-  wander (Star f) = Star (traverse f)
+  wander t = Star <<< t <<< runStar
