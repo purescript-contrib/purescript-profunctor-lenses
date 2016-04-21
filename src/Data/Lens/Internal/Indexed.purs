7d8cd11e5d64a00db615dd0f3245948a79111d4c
tslawler <tslawler@gmail.com>
2016-04-21 19:10:19 -0400

    Rename instances for Indexed; add Wander instance (#30)
    * Rename instances; add Wander instance
    
    Use the {className}{TypeName} convention
    
    * fix typos

--- src/Data/Lens/Internal/Indexed.purs
+++ src/Data/Lens/Internal/Indexed.purs
@@ -9,6 +9,8 @@ import Data.Profunctor.Choice
 import Data.Tuple
 import Data.Either
 
+import Data.Lens.Internal.Wander
+
 -- | Profunctor used for `IndexedOptic`s.
 newtype Indexed p i s t = Indexed (p (Tuple i s) t)
 
@@ -16,13 +18,16 @@ newtype Indexed p i s t = Indexed (p (Tuple i s) t)
 fromIndexed :: forall p i s t. Indexed p i s t -> p (Tuple i s) t
 fromIndexed (Indexed p) = p
 
-instance indexedProfunctor :: (Profunctor p) => Profunctor (Indexed p i) where
+instance profunctorIndexed :: (Profunctor p) => Profunctor (Indexed p i) where
   dimap f g = Indexed <<< dimap (second f) g <<< fromIndexed
 
-instance indexedStrong :: (Strong p) => Strong (Indexed p i) where
+instance strongIndexed :: (Strong p) => Strong (Indexed p i) where
   first = Indexed <<< dimap (\(Tuple i (Tuple a c)) -> (Tuple (Tuple i a) c)) id <<< first <<< fromIndexed
   second = Indexed <<< dimap (\(Tuple i (Tuple c a)) -> (Tuple c (Tuple i a))) id <<< second <<< fromIndexed
 
-instance indexedChoice :: (Choice p) => Choice (Indexed p i) where
+instance choiceIndexed :: (Choice p) => Choice (Indexed p i) where
   left (Indexed p) = Indexed (dimap (\(Tuple i ac) -> either (Left <<< Tuple i) Right ac) id $ left p)
   right (Indexed p) = Indexed (dimap (\(Tuple i ac) -> either Left (Right <<< Tuple i) ac) id $ right p)
+
+instance wanderIndexed :: (Wander p) => Wander (Indexed p i) where
+  wander trav = Indexed <<< wander (\ia2fb (Tuple i s) -> trav (ia2fb <<< Tuple i) s) <<< fromIndexed
