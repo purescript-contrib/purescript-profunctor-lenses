ca87c46f2f43ab8149655bc1c0a0cba5c1368bef
Coot <mszamot@gmail.com>
2017-07-31 18:54:02 +0200

    Fix #67: use purescript-record (#68)

--- src/Data/Lens/Record.purs
+++ src/Data/Lens/Record.purs
@@ -1,47 +1,10 @@
 module Data.Lens.Record (prop) where
 
 import Prelude
-import Data.StrMap as S
+
 import Data.Lens (Lens, lens)
-import Data.Maybe (fromJust)
-import Data.Symbol (class IsSymbol, SProxy, reflectSymbol)
-import Partial.Unsafe (unsafePartial)
-import Unsafe.Coerce (unsafeCoerce)
-
-unsafeGet
-  :: forall r a
-   . String
-  -> Record r
-  -> a
-unsafeGet s = unsafePartial fromJust <<< S.lookup s <<< unsafeCoerce
-
-unsafeSet
-  :: forall r1 r2 a
-   . String
-  -> a
-  -> Record r1
-  -> Record r2
-unsafeSet s a = unsafeCoerce <<< S.insert s a <<< unsafeCoerce
-
-get
-  :: forall r r' l a
-   . IsSymbol l
-  => RowCons l a r' r
-  => SProxy l
-  -> Record r
-  -> a
-get l = unsafeGet (reflectSymbol l)
-
-set
-  :: forall r1 r2 r l a b
-   . IsSymbol l
-  => RowCons l a r r1
-  => RowCons l b r r2
-  => SProxy l
-  -> b
-  -> Record r1
-  -> Record r2
-set l = unsafeSet (reflectSymbol l)
+import Data.Record (get, set)
+import Data.Symbol (class IsSymbol, SProxy)
 
 -- | Construct a (type-changing) lens for a record property, by providing a
 -- | proxy for the `Symbol` which corresponds to the property label.
