593bbaab60be3fcef94fc59e4a7683e69f4a7819
Marcin Szamotulski <mszamot@gmail.com>
2017-09-12 20:02:06 +0200

    both & takeBoth
    * both: combine two `Lens`
    * takeBoth: combine two `Getters`

--- src/Data/Lens/Lens.purs
+++ src/Data/Lens/Lens.purs
@@ -2,6 +2,7 @@
 module Data.Lens.Lens
   ( lens
   , lens'
+  , both
   , withLens
   , cloneLens
   , ilens
@@ -13,16 +14,13 @@ module Data.Lens.Lens
 
 import Prelude
 
-import Data.Lens.Internal.Shop (Shop(..))
 import Data.Lens.Internal.Indexed (Indexed(..))
-import Data.Lens.Types
-  ( Lens,  Lens', ALens, ALens'
-  , IndexedLens, IndexedLens', AnIndexedLens, AnIndexedLens'
-  )
+import Data.Lens.Internal.Shop (Shop(..))
+import Data.Lens.Types (Lens, Lens', ALens, ALens', IndexedLens, IndexedLens', AnIndexedLens, AnIndexedLens')
+import Data.Newtype (un)
 import Data.Profunctor (dimap)
-import Data.Profunctor.Strong (first)
-import Data.Tuple (Tuple(..))
-import Data.Newtype(un)
+import Data.Profunctor.Strong (first, (&&&))
+import Data.Tuple (Tuple(Tuple))
 
 lens' :: forall s t a b. (s -> Tuple a (b -> t)) -> Lens s t a b
 lens' to pab = dimap to (\(Tuple b f) -> f b) (first pab)
@@ -37,6 +35,10 @@ withLens l f = case l (Shop id \_ b -> b) of Shop x y -> f x y
 cloneLens :: forall s t a b. ALens s t a b -> Lens s t a b
 cloneLens l = withLens l \x y p -> lens x y p
 
+both :: forall s a b c d. Lens s s a b -> Lens s s c d -> Lens s s (Tuple a c) (Tuple b d)
+both l r = withLens l
+  \lGet lSet -> withLens r
+    \rGet rSet -> lens (lGet &&& rGet) (\s (Tuple a b) -> rSet (lSet s a) b)
 
 ilens' :: forall i s t a b.
   (s -> Tuple (Tuple i a) (b -> t)) -> IndexedLens i s t a b
