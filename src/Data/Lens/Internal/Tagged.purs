60216358d789bdd8efad09f0fad9dc7c7de46055
Gary Burgess <gary.burgess@gmail.com>
2017-11-07 14:44:15 +0000

    Add `Eq` and `Ord` for `Tagged`

--- src/Data/Lens/Internal/Tagged.purs
+++ src/Data/Lens/Internal/Tagged.purs
@@ -1,9 +1,12 @@
 -- | This module defines the `Tagged` profunctor
 module Data.Lens.Internal.Tagged where
 
+import Prelude
+
 import Data.Either (Either(..))
-import Data.Function (const)
+import Data.Eq (class Eq1)
 import Data.Newtype (class Newtype)
+import Data.Ord (class Ord1)
 import Data.Profunctor (class Profunctor)
 import Data.Profunctor.Choice (class Choice)
 import Data.Profunctor.Closed (class Closed)
@@ -13,6 +16,12 @@ import Data.Tuple (Tuple(..))
 newtype Tagged a b = Tagged b
 
 derive instance newtypeTagged :: Newtype (Tagged a b) _
+
+derive instance eqTagged :: Eq b => Eq (Tagged a b)
+instance eq1Tagged :: Eq1 (Tagged a) where eq1 = eq
+
+derive instance ordTagged :: Ord b => Ord (Tagged a b)
+instance ord1Tagged :: Ord1 (Tagged a) where compare1 = compare
 
 instance taggedProfunctor :: Profunctor Tagged where
   dimap _ g (Tagged x) = Tagged (g x)
