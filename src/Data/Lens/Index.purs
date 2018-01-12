513c61649f0418b1b07e5fcb9b4662d1519bf550
Nicholas Scheel <monoidmusician@gmail.com>
2018-01-12 14:35:36 -0600

    Formulate Index in terms of AffineTraversal'
    Sorry for the ugly duplication with At, maybe we want to put them in the same module? Then index i = at i <<< _Just

--- src/Data/Lens/Index.purs
+++ src/Data/Lens/Index.purs
@@ -7,54 +7,52 @@ import Prelude
 
 import Data.Array as A
 import Data.Identity (Identity)
-import Data.Lens.Internal.Wander (wander)
-import Data.Lens.Types (Traversal')
+import Data.Lens (Prism', _1, _2, _Just, lens, prism')
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Types (AffineTraversal')
 import Data.Map as M
-import Data.Maybe (Maybe, maybe, fromMaybe)
+import Data.Maybe (Maybe(..), maybe)
 import Data.Set as S
 import Data.StrMap as SM
-import Data.Traversable (traverse)
+import Data.Tuple.Nested (Tuple3, tuple3, uncurry3)
 
 class Index m a b | m -> a, m -> b where
-  ix :: a -> Traversal' m b
+  ix :: a -> AffineTraversal' m b
 
-instance indexArr :: Eq i => Index (i -> a) i a where
-  ix i =
-    wander \coalg f ->
-      coalg (f i) <#> \a j ->
-        if i == j then a else f j
+instance indexFn :: Eq i => Index (i -> a) i a where
+  ix i = lens (\f -> f i) \f a j -> if i == j then a else f j
 
 instance indexMaybe :: Index (Maybe a) Unit a where
-  ix _ = wander traverse
+  ix _ = _Just
 
 instance indexIdentity :: Index (Identity a) Unit a where
-  ix _ = wander traverse
+  ix _ = _Newtype
 
 instance indexArray :: Index (Array a) Int a where
-  ix n =
-    wander \coalg xs ->
-      xs A.!! n #
-        maybe
-          (pure xs)
-          (coalg >>> map \x -> fromMaybe xs (A.updateAt n x xs))
+  ix n = separateIndex <<< _2 <<< _1
+    where
+      separateIndex :: Prism' (Array a) (Tuple3 (Array a) a (Array a))
+      separateIndex = prism'
+        (uncurry3 \l e r -> l <> [e] <> r)
+        \as -> A.index as n <#> \e -> tuple3
+          (A.take n as) e (A.drop (n+1) as)
 
 instance indexSet :: Ord a => Index (S.Set a) a Unit where
-  ix x =
-    wander \coalg ->
-      pure <<< S.insert x
+  ix x = lens get (flip update) <<< _Just
+    where
+      get xs =
+        if S.member x xs
+           then Just unit
+           else Nothing
+      update Nothing = S.delete x
+      update (Just _) = S.insert x
 
 instance indexMap :: Ord k => Index (M.Map k v) k v where
-  ix k =
-    wander \coalg m ->
-      M.lookup k m #
-        maybe
-          (pure m)
-          (coalg >>> map \v -> M.insert k v m)
+  ix k = _Just >>>
+    lens (M.lookup k) \m ->
+      maybe (M.delete k m) \v -> M.insert k v m
 
 instance indexStrMap :: Index (SM.StrMap v) String v where
-  ix k =
-    wander \coalg m ->
-      SM.lookup k m #
-        maybe
-          (pure m)
-          (coalg >>> map \v -> SM.insert k v m)
+  ix k = _Just >>>
+    lens (SM.lookup k) \m ->
+      maybe (SM.delete k m) \ v -> SM.insert k v m
