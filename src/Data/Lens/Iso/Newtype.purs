9be40c00d6fc5b486192430a25c07aab962abcb6
Phil Freeman <paf31@cantab.net>
2020-08-10 19:27:25 -0700

    Add unto (#93)
    Co-authored-by: Thomas Honeyman <admin@thomashoneyman.com>

--- src/Data/Lens/Iso/Newtype.purs
+++ src/Data/Lens/Iso/Newtype.purs
@@ -1,6 +1,6 @@
 module Data.Lens.Iso.Newtype where
 
-import Data.Lens.Iso (Iso, iso)
+import Data.Lens.Iso (Iso, Iso', iso)
 import Data.Newtype (class Newtype, wrap, unwrap)
 
 -- | An Iso between a newtype and its inner type.
@@ -11,3 +11,9 @@ import Data.Newtype (class Newtype, wrap, unwrap)
 _Newtype :: forall t a s b. Newtype t a => Newtype s b => Iso t s a b
 _Newtype = iso unwrap wrap
 
+-- | A variant of `_Newtype` which takes the constructor as an argument
+-- | and infers its inverse.
+-- |
+-- | This is useful as an aid to type inference in certain situations.
+unto :: forall n o. Newtype n o => (o -> n) -> Iso' n o
+unto _ = _Newtype
