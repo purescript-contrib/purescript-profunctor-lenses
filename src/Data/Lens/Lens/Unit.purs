83034a64058456531239a5889a1c1bf103e5d585
neppord <neppord@gmail.com>
2021-09-05 10:21:17 +0200

    Document united
    I just wanted to make a small contribution to get going

--- src/Data/Lens/Lens/Unit.purs
+++ src/Data/Lens/Lens/Unit.purs
@@ -5,5 +5,11 @@ import Prelude
 import Data.Lens.Lens (Lens', lens)
 
 -- | There is a `Unit` in everything.
+-- | ```purescript
+-- | > view united [1,2,3]
+-- | Unit
+-- | > over united (\a -> a) [1,2,3]
+-- | [1 2 3]
+-- | ```
 united :: forall a. Lens' a Unit
 united = lens (const unit) const
