a0720d313222c703bf6f1fd0feae20e37ed01d1c
Jordan Martinez <jordanalex.martinez@protonmail.com>
2021-09-21 06:09:05 -0700

    Add module docs that refer to docs folder

--- src/Data/Lens/Types.purs
+++ src/Data/Lens/Types.purs
@@ -1,4 +1,11 @@
 -- | This module defines types for working with lenses.
+-- |
+-- | All optics have their normal name (e.g. `Lens`)
+-- | and one whose name is prefixed with either "A"
+-- | or "An" (e.g. `ALens`). Prefixed versions avoid the
+-- | issue of "impredicativity". To understand that concept
+-- | more and why prefixed names are sometimes necessary,
+-- | see the `./docs` folder.
 module Data.Lens.Types
   ( module Data.Lens.Types
   , module Data.Lens.Internal.Exchange
